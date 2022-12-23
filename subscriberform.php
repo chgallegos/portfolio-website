0f8a7b1a40307826679c3929d1d830a2f0f7ef6f
ChrisG <chrisale89@gmail.com>
2022-12-23 13:17:19 -0700

    website color change

---
+++ subscriberform.php
@@ -0,0 +1,16 @@
+<?php
+
+
+if($_POST["message"]) {
+
+
+mail("chrisale89@gmail.com", "Message from Portfolio site",
+
+
+$_POST["insert your message here"]. "From: an@email.address");
+
+
+}
+
+
+?>
